7febbdf2d15d2948b0ca2a33c62619d7f4022338
Pint <83248746+janekv20@users.noreply.github.com>
2021-07-11 21:42:59 -0500

    updated generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,73 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+    if (license !== 'no license') {
+        return '';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+    if (license !== 'no license') {
+        return '';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+    if (license !== 'no license') {
+        return '';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
 
+    ${renderLicenseBadge(data.license)}
+
+    ## Table-of-Contents
+
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [Contribution Guidelines](#contribution)
+    * [Testing Instructions](#testing)
+    * [Contact](#contact)
+
+    ## [Description](#table-of-contents)
+
+    ${data.description}
+    
+    ## [Installation](#table-of-contents)
+
+    ${data.installation}
+
+    ## [Usage](#table-of-contents)
+
+    ${data.usage}
+
+    ## [Contribution Guidelines](#table-of-contents)
+
+    ${data.contribution}
+
+    ## [Testing Instructions](#table-of-contents)
+
+    ${data.testing}
+
+    ## [Contact](#table-of-contents)
+
+   [GitHub Repository](https://github.com/${data.github})
+
+   [Email Address: ${data.email}](mailto:${data.email})
 `;
 }
 
